112ac738a94f8aede42622a4c097aeac809de10e
Or7ando <orlando_cau@gmx.fr>
2015-06-28 17:23:58 +0200

    Modif
    ajout de fonction

--- maths-master/js/matrices.js
+++ maths-master/js/matrices.js
@@ -4,23 +4,6 @@ var genbutton = document.getElementById('genButton');
 var maincontainer = document.getElementById('mainContainer');
 
 
-MT.matriceadd = function(matrice1,matrice2){
-    console.log(matrice1.n);
-    this.n = matrice1.n;
-    this.p = matrice1.p;
-    this.array = new Int8Array((this.n)*(this.p));
-    for(var i=0;i<this.n;i++) {
-    for(var j=0;j<this.p;j++) {
-        var c = matrice1.array[i+this.n*j]+matrice2.array[i+this.n*j];
-        //var c = Math.floor(19*Math.random()-9);
-        this.array[i+this.n*j] = c;
-    }
-    }
-    this.div = document.createElement('div');
-    this.div.classList.add('matriceDiv');
-
-}
-
 MT.matrice = function(n,p) {
     this.n = n ? n : Math.floor(9*Math.random()+1);
     this.p = p ? p : this.n;
@@ -36,6 +19,7 @@ MT.matrice = function(n,p) {
     this.div = document.createElement('div');
     this.div.classList.add('matriceDiv');
 }
+
 MT.matrice.prototype.genCoeff = function() {
     return Math.floor(19*Math.random()-9);
 }
@@ -60,6 +44,16 @@ MT.matrice.prototype.genLatex = function() {
     this.div.appendChild(this.latexPre);
     return this.latexString;
 }
+
+MT.matrice.prototype.show = function(){
+    this.genLatex();
+    maincontainer.appendChild(this.div);
+    this.genMathJax();
+
+
+}
+
+
 MT.matrice.prototype.genMathJax = function() {
     this.mathJaxContainer = document.createElement('div');
     this.div.appendChild(this.mathJaxContainer);
@@ -67,30 +61,17 @@ MT.matrice.prototype.genMathJax = function() {
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
 
-MT.matriceadd.prototype.genLatex = function() {
-    this.latexString = '\\begin{pmatrix}\n';
-    this.latexPre = document.createElement('pre');
+MT.matrice.prototype.add = function(matrice1,matrice2) { 
     for(var i=0;i<this.n;i++) {
     for(var j=0;j<this.p;j++) {
-        var c = this.array[i+this.n*j];
-        if(c >=0 ) this.latexString += ' ';
-        this.latexString += c
-        if(j < (this.p-1)) this.latexString += ' & '
-        else this.latexString += ' ';
+        var c = matrice1.array[i+this.n*j]+matrice2.array[i+this.n*j];
+        //var c = Math.floor(19*Math.random()-9);
+        this.array[i+this.n*j] = c;
     }
-    this.latexString += '\\\\\n'
     }
-    this.latexString += '\\end{pmatrix}';
-    this.latexPre.textContent = this.latexString;
-    this.div.appendChild(this.latexPre);
-    return this.latexString;
-}
-MT.matriceadd.prototype.genMathJax = function() {
-    this.mathJaxContainer = document.createElement('div');
-    this.div.appendChild(this.mathJaxContainer);
-    this.mathJaxContainer.innerHTML = '$$'+this.latexString+'$$';
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-}
+
+
+} 
 
 
 
@@ -98,18 +79,10 @@ MT.matriceadd.prototype.genMathJax = function() {
 genbutton.addEventListener('click', function() {
     var matrice1 = new MT.matrice(3,3);
     var matrice2 = new MT.matrice(3,3);
-    var matrice = new MT.matriceadd(matrice1,matrice2);
-    console.log(matrice);
+    var matrice = new MT.matrice(3,3);
     
-    matrice1.genLatex();
-    maincontainer.appendChild(matrice1.div);
-    matrice1.genMathJax();
-    
-    matrice2.genLatex();
-    maincontainer.appendChild(matrice2.div);
-    matrice2.genMathJax();
-    
-    matrice.genLatex();
-    maincontainer.appendChild(matrice.div);
-    matrice.genMathJax();
+    matrice1.show();
+    matrice2.show();
+    matrice.add(matrice1,matrice2);
+    matrice.show();
 });
